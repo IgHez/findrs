d2a52ede2f198b8424cb9df93b38bf5b34089580
IgHez <109860992+IgHez@users.noreply.github.com>
2023-09-29 00:52:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobile-menu');
-const navLinks = document.querySelectorAll('.mobile-links');
+// dataHandler.js
 
-// Function to open the mobile menu
-function openMobileMenu() {
-    mobileMenu.classList.remove('d-none');
+// Function to append data to an array and stringify it
+function appendDataAndStringify(dataArray, newData) {
+    dataArray.push(newData);
+    return JSON.stringify(dataArray);
 }
-
-// Function to close the mobile menu
-function closeMobileMenu() {
-    mobileMenu.classList.add('d-none');
-}
-
-hamburger.addEventListener('click', () => {
-    if (mobileMenu.classList.contains('d-none')) {
-        openMobileMenu();
-    } else {
-        closeMobileMenu();
-    }
-});
-
-// Function to close the mobile menu when links are clicked
-navLinks.forEach((navLink) => {
-    navLink.addEventListener('click', () => {
-        closeMobileMenu(); // Close the mobile menu when a link is clicked
-    });
-});
